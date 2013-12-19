7d9f44260df7d9cd5e7cfbc0fa828185e58c4cd2
Daniel Gong <daniel@strikingly.com>
2013-12-19 17:12:11 +0800

    add client side flow for linkedin auth

--- lib/omniauth/strategies/linkedin.rb
+++ lib/omniauth/strategies/linkedin.rb
@@ -25,7 +25,7 @@ module OmniAuth
       uid { raw_info['id'] }
 
       info do
-        {
+        prune! {
           :name => user_name,
           :email => raw_info['emailAddress'],
           :nickname => user_name,
@@ -41,7 +41,9 @@ module OmniAuth
       end
 
       extra do
-        { 'raw_info' => raw_info }
+        hash = {}
+        hash['raw_info'] = raw_info unless skip_info?
+        prune! hash
       end
 
       alias :oauth2_access_token :access_token
@@ -56,14 +58,80 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get("/v1/people/~:(#{options.fields.join(',')})?format=json").parsed
+        @raw_info ||= access_token.get("/v1/people/~:(#{options.fields.join(',')})?format=json").parsed || {}
+      end
+
+      def build_access_token
+        if access_token = request.params["access_token"]
+          ::OAuth2::AccessToken.from_hash(client, {"access_token" => access_token})
+        else
+          super
+        end
+      end
+
+      def request_phase
+        if request_contains_secure_cookie?
+          params[:access_token]  = secure_cookie['access_token']
+          query = Rack::Utils.build_query(params)
+
+          url = callback_url
+          url << "?" unless url.match(/\?/)
+          url << "&" unless url.match(/[\&\?]$/)
+          url << query
+
+          redirect url
+        else
+          super
+        end
+      end
+
+      def secure_cookie
+        @secure_cookie ||= raw_secure_cookie && parse_secure_cookie(raw_secure_cookie)
       end
 
       private
 
+      def raw_secure_cookie
+        request.cookies["linkedin_oauth_#{client.id}"]
+      end
+
+      def request_contains_secure_cookie?
+        secure_cookie && secure_cookie['access_token']
+      end
+
+      def parse_secure_cookie(cookie)
+        payload = JSON.parse cookie
+        if valid_signature(client.secret, payload)
+          payload
+        end
+      end
+
+      def validate_signature(secret, payload)
+        valid = false
+        if payload['signature_version'] == '1'
+          if payload['signature_order'].present? and payload['signature_order'].is_a?(Array)
+            plain_msg = payload['signature_order'].map {|key| payload[key]}.join('')
+            if payload['signature_method'] == 'HMAC-SHA1'
+              signature = Base64.encode64(OpenSSL::HMAC.digest('sha1', client.secret, plain_msg)).chomp
+              if signature == payload['signature']
+                valid = true
+              end
+            end
+          end
+        end
+        valid
+      end
+
       def user_name
         name = "#{raw_info['firstName']} #{raw_info['lastName']}".strip
         name.empty? ? nil : name
+      end
+
+      def prune!(hash)
+        hash.delete_if do |_, value|
+          prune!(value) if value.is_a?(Hash)
+          value.nil? || (value.respond_to?(:empty?) && value.empty?)
+        end
       end
     end
   end
