dc61803e148377222fc538071615101b367164da
Niandalu <niandalu@gmail.com>
2015-03-24 11:12:03 +0800

    rename provider

--- lib/omniauth/strategies/linkedin_oauth2.rb
+++ lib/omniauth/strategies/linkedin_oauth2.rb
@@ -4,7 +4,7 @@ module OmniAuth
   module Strategies
     class LinkedInOAuth2 < OmniAuth::Strategies::OAuth2
       # Give your strategy a name.
-      option :name, 'linkedin'
+      option :name, 'linkedin_oauth2'
 
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
